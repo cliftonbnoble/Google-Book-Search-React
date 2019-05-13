3ed5833843c726db5a9f3729670ee5da535bb75b
Clifton Noble <cliftonnoble@cliftons-imac.lan>
2019-05-12 17:00:23 -0700

    adding base files from class exercise

--- README.md
+++
@@ -1,2 +0,0 @@
-# Google-Book-Search-React
-Google book search app built using React
---
+++ client/build/precache-manifest.024f6f0ef579616b59846e1dc4b8f782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2cf91f949a26dd3b29f",
+    "url": "/static/css/main.ee551f88.chunk.css"
+  },
+  {
+    "revision": "f2cf91f949a26dd3b29f",
+    "url": "/static/js/main.397ff19d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "4d7b2c3d1a712885b3d8f5b92f9e7653",
+    "url": "/index.html"
+  }
+];
