6917b398099febf86f9fa129ef454d9c229ff2d7
Clifton Noble <cliftonnoble@cliftons-mbp.lan>
2019-05-19 21:40:51 -0700

    adding fixes for save, logo, app

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -5,8 +5,8 @@ function Logo() {
     return (
             <div className="logo">
                 <img className="logo-position" src="http://www.cliftonnoble.com/wp-content/uploads/2019/05/book-emoji.png" alt="Book Icon"/>
-                <h1 className="logo-text logo-position">Google Book Search </h1>
-                <h5 className="logo-text logo-position">Built With React</h5>
+                <h1 className="logo-text logo-position google-book-search">Google Book Search </h1>
+                <h5 className="logo-text logo-position book-search-small">Built With React</h5>
             </div>
     )
 }
